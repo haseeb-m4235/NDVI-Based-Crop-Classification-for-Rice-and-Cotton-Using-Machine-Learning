5e4d8e516c83f70e2f10b96d395dd6342fa1a9a3
haseeb-m4235 <haseebmuhammad4235@gmail.com>
2024-12-18 00:18:03 +0500

    DocString update

--- DataSplit.py
+++ DataSplit.py
@@ -3,10 +3,25 @@ import pandas as pd
 
 class DataSplit():
     def __init__(self, datasetDir):
+        """
+        Args:
+            datasetDir (str): Path to the dataset directory.
+        """
         self.datasetDir = datasetDir
         self.combining_data()
         
     def combining_data(self):
+        """
+        Combines the data from the different csv files into one dataframe.
+
+        The cotton and rice data are combined into one dataframe with the 'label' column
+        indicating whether it is a cotton (1) or rice (0) sample and the 'year' column indicating
+        the year of the data (2021, 2022 or 2023). The combined dataframe is stored as an
+        instance variable.
+
+        Returns:
+            None
+        """
         cotton_2021_path = os.path.join(self.datasetDir, "Cotton", "cotton2021.csv")
         cotton_2022_path = os.path.join(self.datasetDir, "Cotton", "cotton2022.csv")
         cotton_2023_path = os.path.join(self.datasetDir, "Cotton", "cotton2023.csv")
@@ -37,6 +52,20 @@ class DataSplit():
         self.combined_data = pd.concat([cotton_2021_df, rice_2021_df, cotton_2022_df, rice_2022_df, cotton_2023_df, rice_2023_df], axis=0)
 
     def get_split(self, train_data_1, train_data_2, test_data):
+        """ 
+        Splits the combined data into a training set and a testing set.
+
+        Args:
+            train_data_1 (int): The year of the first set of training data.
+            train_data_2 (int): The year of the second set of training data.
+            test_data (int): The year of the testing data.
+
+        Returns:
+            A tuple of four dataframes: X_train, y_train, X_test, y_test. X_train and X_test are the feature data for
+            the training and testing sets, respectively. y_train and y_test are the labels for the training and testing
+            sets, respectively.
+        """
+
         train_df = self.combined_data[(self.combined_data['year'] == train_data_1) | (self.combined_data['year'] == train_data_2)]
         test_df = self.combined_data[(self.combined_data['year'] == test_data)]
         
